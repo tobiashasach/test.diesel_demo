beef04ebf263c6f7d163bb292c60e70858ed6e24
tobias.hasach <contactme.tobias.hasach@gmail.com>
2024-12-13 12:11:49 +0100

    feat: add show posts

---
+++ README.md
@@ -0,0 +1,12 @@
+# test.diesel_demo
+
+## Setup
+
+1. Run `cargo build`
+2. Install diesel CLI https://diesel.rs/guides/getting-started
+3. Run `cp .env.example .env`
+4. Run `docker compose up`
+5. Run `diesel setup`
+6. Ubuntu: Run `sudo apt install libpq-dev` [Issue](https://stackoverflow.com/questions/62708607/how-to-fix-diesel-cli-link-libpq-lib-error-with-postgres-tools-installed-in-dock)
+
+Based on: https://diesel.rs/guides/getting-started
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod models;
+pub mod schema;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
